051d3432b42955ac07fd6f89a34710178cf82ff2
Meirzhan <meirzhan250gmail.com>
2025-12-06 19:22:55 +0500

    homework-5: refactoring code

--- src/main/kotlin/io/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/snowflakegenerator/SnowflakeController.kt
@@ -1,19 +1,23 @@
 package io.snowflakegenerator
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/v1")
 class SnowflakeController(private val generator: Generator) {
+	private val logger = KotlinLogging.logger {}
+
 	@GetMapping("/next-id")
 	suspend fun generate(): Map<String, Any> {
 		val id = generator.nextId()
+		val podName = System.getenv("POD_NAME")
 
-		println("POD_NAME: ${System.getenv("POD_NAME")}")
+		logger.info("POD_NAME: $podName")
 
 		return mapOf(
 			"id" to id,
-			"pod" to System.getenv("POD_NAME")
+			"pod" to podName
 		)
 	}
 }
